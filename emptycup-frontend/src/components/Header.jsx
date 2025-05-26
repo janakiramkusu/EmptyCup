04c0c7b1f8f9e7a323cff37662e7ef49023a8893
janakiramkusu <160325078+janakiramkusu@users.noreply.github.com>
2025-05-26 15:12:17 +0530

    Update Header.jsx

--- emptycup-frontend/src/components/Header.jsx
+++ emptycup-frontend/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import './Header.css';
 import { BsThreeDotsVertical } from 'react-icons/bs';
+import logoImg from '../assets/logo-small 1.png';
 
 const Header = () => {
   return (
     <header className="header">
-      <img src="src\assets\logo-small 1.png" alt="Logo" className="logo" />
+      <img src={logoImg} alt="Logo" className="logo" />
       <h1 className="title">EmptyCup</h1>
       <BsThreeDotsVertical className="menu-icon" />
     </header>
