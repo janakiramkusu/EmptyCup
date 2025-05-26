a6a1c4c00774be12df87798628c5e0048bc6ed62
janakiramkusu <160325078+janakiramkusu@users.noreply.github.com>
2025-05-26 13:32:38 +0530

    Update Navbar.jsx

--- emptycup-frontend/src/components/Navbar.jsx
+++ emptycup-frontend/src/components/Navbar.jsx
@@ -1,29 +1,34 @@
 import "./Navbar.css";
+import contactImg from "../assets/contact.png";
+import galleryImg from "../assets/Gallery.png";
+import mapsImg from "../assets/Maps.png";
+import shortlistedImg from "../assets/Shortlisted.png";
+import sortImg from "../assets/sort.png";
 
 const Navbar = ({ isShortlistedFilter, toggleShortlistedFilter }) => {
   return (
     <nav className="navbar">
       <div className="nav-left">
         <div className="nav-item active">
-          <img src="src/assets/contact.png" alt="Contacts" />
+          <img src={contactImg} alt="Contacts" />
         </div>
         <div className="nav-item">
-          <img src="./public/Gallery.png" alt="Gallery" />
+          <img src={galleryImg} alt="Gallery" />
         </div>
         <div className="nav-item disabled">
-          <img src="src/assets/Maps.png" alt="Map" />
+          <img src={mapsImg} alt="Map" />
         </div>
       </div>
 
       <div className="nav-right">
         <div
-          className={`nav-item ${isShortlistedFilter ? 'active' : ''}`}
+          className={`nav-item ${isShortlistedFilter ? "active" : ""}`}
           onClick={toggleShortlistedFilter}
         >
-          <img src="src/assets/Shortlisted.png" alt="Shortlisted" />
+          <img src={shortlistedImg} alt="Shortlisted" />
         </div>
         <div className="nav-item">
-          <img src="src/assets/sort.png" alt="Sort" />
+          <img src={sortImg} alt="Sort" />
         </div>
       </div>
     </nav>
